ffca39cdf2ca35c7a04dee05184cfc104dc86203
Filip Pokryvka <fpokryvk@redhat.com>
2019-09-16 09:14:11 +0200

    fix version_control.py

--- version_control.py
+++ version_control.py
@@ -2,19 +2,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import sys
 from subprocess import call, check_output
 
-def skip_non_default_packages(tags):
-    if 'not_with_rhel7_pkg' in tags:
-        # Do not run on stock RHEL7 package
-        if call('rpm -qi NetworkManager |grep -q build.*bos.redhat.co', shell=True) == 0 and \
-        check_output("rpm --queryformat %{RELEASE} -q NetworkManager |awk -F .  '{ print ($1 < 200) }'", shell=True).decode('utf-8').strip() == '1' and \
-        call("grep -q 'release 7' /etc/redhat-release", shell=True) == 0:
-            return True
-        else:
-            return False
-    else:
-        return False
-
-current_nm_version = "".join(check_output("""NetworkManager -V |awk 'BEGIN { FS = "." }; {printf "%03d%03d%03d", $1, $2, $3}'""", shell=True).decode('utf-8').split('-')[0])
+current_nm_version = [ int(x) for x in check_output(["NetworkManager","-V"]).decode("utf-8").split("-")[0].split(".") ]
 
 if "NetworkManager" in sys.argv[2] and "Test" in sys.argv[2]:
     test_name = "".join('_'.join(sys.argv[2].split('_')[2:]))
@@ -22,8 +10,6 @@ else:
     test_name = sys.argv[2]
 
 raw_tags = check_output ("behave $(grep -l @%s %s/features/*.feature) -k -t %s --dry-run |grep %s" %(test_name, sys.argv[1], test_name, test_name), shell=True).decode('utf-8')
-tests_tags = raw_tags.split('\n')
-
 tests_tags = raw_tags.split('\n')
 
 # for every line with the same test_name
