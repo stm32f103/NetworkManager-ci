b568f099efa5dd945893e001197ee5b4ab030541
Vladimír Beneš <vbenes@redhat.com>
2020-05-14 10:16:04 +0200

    env: add scapy python3 to s390x

--- prepare/envsetup.sh
+++ prepare/envsetup.sh
@@ -260,6 +260,7 @@ install_el7_packages () {
         yum -y install gcc python3-devel cairo-gobject-devel pygobject3-devel cairo-devel cairo pycairo
         python3 -m pip install dbus-python
         python3 -m pip install PyGObject
+        python3 -m pip install scapy
     fi
 
     yum -y install git iw net-tools wireshark psmisc bridge-utils firewalld dhcp ethtool python36-dbus python36-gobject dnsmasq NetworkManager-vpnc
