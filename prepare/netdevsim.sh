494c433431db3c33bef8470d3b21f2ae5de436fe
Vladimír Beneš <vbenes@redhat.com>
2020-10-20 12:06:05 +0200

    fix coal and ring to work with 240.5 and also 240 version like kernels

--- prepare/netdevsim.sh
+++ prepare/netdevsim.sh
@@ -2,7 +2,11 @@
 
 function setup () {
     MAJOR="$(uname -r |awk -F '-' '{print $1}')"
-    MINOR="$(uname -r |awk -F '-' '{print $2}'|awk -F '.' '{print  $1"."$2"."$3}')"
+    if test $(uname -r |awk -F '-' '{print $2}'|awk -F '.' '{print $3}') == $(arch); then
+        MINOR="$(uname -r |awk -F '-' '{print $2}'|awk -F '.' '{print  $1"."$2}')"
+    else
+        MINOR="$(uname -r |awk -F '-' '{print $2}'|awk -F '.' '{print  $1"."$2"."$3}')"
+    fi
     LINUX=linux-$MAJOR-$MINOR
     # We need this patched netdevsim device to support ring/coal ethtool options
     PATCH="0001-netdevsim-add-mock-support-for-coalescing-and-ring-o-1.patch"
