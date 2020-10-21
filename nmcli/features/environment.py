2d42fad550461fd7b494c6681b3f6b93486337f3
Vladimír Beneš <vbenes@redhat.com>
2020-10-21 13:38:08 +0200

    we need to remove testeth0 if it still exists

--- nmcli/features/environment.py
+++ nmcli/features/environment.py
@@ -491,6 +491,8 @@ def delete_old_lock(dir, lock):
 
 def restore_testeth0():
     print ("* restoring testeth0")
+    call("nmcli con delete testeth0 2>&1 > /dev/null", shell=True)
+
     if not os.path.isfile('/tmp/nm_plugin_keyfiles'):
         # defaults to ifcfg files (RHELs)
         call("yes 2>/dev/null | cp -rf /tmp/testeth0 /etc/sysconfig/network-scripts/ifcfg-testeth0", shell=True)
